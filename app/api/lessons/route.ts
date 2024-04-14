2bf0b87562be9307823835c6f8abfa25855756b9
KiranPK12 <kiranpadma2003@gmail.com>
2024-04-14 15:53:15 +0530

    dev initial commit

--- app/api/lessons/route.ts
+++ app/api/lessons/route.ts
@@ -8,22 +8,18 @@ export const GET = async () => {
     if (!isAdmin()) {
         return new NextResponse("Unauthorized", { status: 401 })
     }
-
     const data = await db.query.lessons.findMany();
     return NextResponse.json(data);
 }
-
 
 export const POST = async (req: Request) => {
     if (!isAdmin()) {
         return new NextResponse("Unauthorized", { status: 401 })
     }
-
     const body = await req.json();
     const data = await db.insert(lessons).values({
         ...body,
     }).returning()
-
     return NextResponse.json(data[0]);
 }
 
